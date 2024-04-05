a88623f9c179d3718f9c353550bdeb2bc907a199
ethan <ethan@zolplay.com>
2024-04-05 13:02:05 +0800

    fix: project entry

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup'
 
 export default defineConfig((options) => ({
   treeshake: true,
   splitting: true,
-  entry: ["packages/core"],
-  format: ["esm"],
+  entry: ['packages'],
+  format: ['esm'],
   clean: true,
   dts: true,
   minify: true,
   ...options,
-}));
+}))
