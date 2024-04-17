3ec72bfe84216b5913dd6aee91c5595b7ad8870a
Ethan <ethan@zolplay.com>
2024-04-18 01:01:42 +0800

    feat: add chat endpoint

--- packages/chat/completions.ts
+++ packages/chat/completions.ts
@@ -9,20 +9,85 @@ export class Completions extends APIResource {
 
       let prompt = ''
 
-      messages.map((item) => {
-        if (item.role === 'system') {
-          prompt += '<|im_start|>system' + item.content + '<|im_end|>' + '\n'
-        } else if (item.role === 'assistant') {
-          prompt += '<|im_start|>assistant' + item.content + '<|im_end|>' + '\n'
-        } else if (item.role === 'user') {
-          prompt += '<|im_start|>user' + item.content + '<|im_end|>' + '\n'
+      if (model.startsWith('openhermes')) {
+        messages.map((item) => {
+          if (item.role === 'system') {
+            prompt += '<|im_start|>system' + item.content + '<|im_end|>' + '\n'
+          } else if (item.role === 'assistant') {
+            prompt +=
+              '<|im_start|>assistant' + item.content + '<|im_end|>' + '\n'
+          } else if (item.role === 'user') {
+            prompt += '<|im_start|>user' + item.content + '<|im_end|>' + '\n'
+          }
+        })
+      } else if (model.startsWith('meta-llama')) {
+        const role_dict: any = {
+          system: {
+            pre_message: '[INST] <<SYS>>\n',
+            post_message: '\n<</SYS>>\n [/INST]\n',
+          },
+          user: {
+            pre_message: '[INST] ',
+            post_message: ' [/INST]\n',
+          },
+          assistant: {
+            post_message: '\n',
+          },
         }
-      })
 
-      const id = Randomstring.generate({
-        charset: 'hex',
-        length: 10,
-      })
+        messages.map((item: any) => {
+          const role = item.role
+
+          let pre_message_str = role_dict[role].pre_message
+          let post_message_str = role_dict[role].post_message
+
+          prompt += `${pre_message_str}${item.content}${post_message_str}`
+
+          if (role === 'assistant') {
+            prompt += '</s>'
+          }
+        })
+      } else if (model.startsWith('mistralai')) {
+        if (messages.length === 1) {
+          prompt = `[INST] ${messages[0]['content']} [/INST]`
+        } else {
+          const role = messages[messages.length - 1].role
+          const previous_round_messages =
+            role === 'user' ? messages.slice(0, messages.length - 1) : messages
+
+          const role_dict: any = {
+            system: {
+              pre_message: '[INST] \n',
+              post_message: ' [/INST]\n',
+            },
+            user: {
+              pre_message: '[INST] ',
+              post_message: ' [/INST]\n',
+            },
+            assistant: {
+              pre_message: ' ',
+              post_message: '</s>',
+            },
+          }
+
+          prompt += '<s>'
+
+          previous_round_messages.map((item: any) => {
+            const pre_message_str = role_dict[role].pre_message
+            const post_message_str = role_dict[role].post_message
+
+            prompt += `${pre_message_str}${item.content}${post_message_str}`
+          })
+
+          prompt += '</s>'
+
+          if (role === 'user') {
+            prompt += `[INST] ${messages[messages.length - 1]['content']} [/INST]`
+          }
+        }
+      }
+
+      const id = Randomstring.generate({ charset: 'hex', length: 10 })
 
       const model_input = {
         prompt,
@@ -90,7 +155,6 @@ export class Completions extends APIResource {
         },
         ended: Math.floor(Date.now() / 1000),
       }
-      console.log(result, 'result')
       return result
     } catch (error) {
       console.log(error.message, 'Completions chat error')
@@ -100,6 +164,16 @@ export class Completions extends APIResource {
     }
   }
 }
+
+export type ChatCompletionModel =
+  | (string & {})
+  | 'mistralai/mixtral-8x7b-instruct-v0.1'
+  | 'mistralai/mistral-7b-instruct-v0.2'
+  | 'openhermes-2.5-mistral-7b-gptq'
+  | 'openhermes-2-pro-mistral-7b'
+  | 'openhermes-mixtral-8x7b-gptq'
+  | 'openhermes-2-yi-34b-gptq'
+  | 'meta-llama/llama-2-70b-chat'
 
 export interface ChatCompletionMessageParam {
   /**
@@ -128,15 +202,7 @@ export interface ChatCompletionCreateParamsNonStreaming {
   /**
    * ID of the model to use.
    */
-  model:
-    | (string & {})
-    | 'mistralai/mixtral-8x7b-instruct-v0.1'
-    | 'mistralai/mistral-7b-instruct-v0.2'
-    | 'openhermes-2.5-mistral-7b-gptq'
-    | 'openhermes-2-pro-mistral-7b'
-    | 'openhermes-mixtral-8x7b-gptq'
-    | 'openhermes-2-yi-34b-gptq'
-    | 'meta-llama/llama-2-70b-chat'
+  model: ChatCompletionModel
 
   /**
    * What sampling temperature to use, between 0 and 2. Higher values like 0.8 will
